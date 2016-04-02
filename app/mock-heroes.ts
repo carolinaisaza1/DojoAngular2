fb33bd5bbcb307317b93c0ce1a059abb3881fe90
carolinaisaza1 <carito9994@gmail.com>
2016-04-02 12:35:24 -0500

    DojoAngular2

---
+++ app/mock-heroes.ts
@@ -0,0 +1,18 @@
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
+
+import {Hero} from './hero';
+
+export var Heroes: Hero[]=[
+	{"id":11, "name": "Batman"},
+	{"id":12, "name": "SuperMan"},
+	{"id":13, "name": "Flash"},
+	{"id":14, "name": "Thor"},
+	{"id":15, "name": "Iron Man"},
+	{"id":16, "name": "Capitan America"}
+
+];
